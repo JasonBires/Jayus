e1a2a05ed263a1faff3e7ce683248868cfddfd14
Jason Bires <jason.m.bires@gmail.com>
2014-07-23 20:38:05 -0400

    Updated timecontrol code, removed buffer dump and improved efficiency.

--- Jayus/Assets/Scripts/CameraMover.cs
+++ Jayus/Assets/Scripts/CameraMover.cs
@@ -2,60 +2,50 @@
 using System.Collections;
 
 public class TimeController {
-	private Vector3[] posStorage;
 	private Vector3[] posStorageBuffer;
-	private Vector3[] rotStorage;
 	private Vector3[] rotStorageBuffer;
-	private bool hasBeenDumped = false;
-
-	private int size = 0;
-	private int storeSize = 1599;
-	private const int MAX_SIZE = 1600;
+	private int size;
+	private const int MAX_SIZE = 3200;
+	private Vector3 SIGNAL_EMPTY = new Vector3 (-9000, 1000, -90000);
 
 	public TimeController() {
-		posStorageBuffer = new Vector3[1600];
-		rotStorageBuffer = new Vector3[1600];
-	}
-
-	private bool isFull() {
-		return size == MAX_SIZE;
-	}
-
-	public bool hasGottenDumped() {
-		return hasBeenDumped;
-	}
-
-	private void bufferDump() {
-		posStorage = posStorageBuffer;
-		rotStorage = rotStorageBuffer;
-		hasBeenDumped = true;
+		posStorageBuffer = new Vector3[MAX_SIZE];
+		for (int i = 0; i < MAX_SIZE; i++) {
+			posStorageBuffer [i] = SIGNAL_EMPTY;
+		}
+		rotStorageBuffer = new Vector3[MAX_SIZE];
 		size = 0;
-		storeSize = 1599;
+	}
+	//Set a temp variable for size-- 
+	public bool isEmpty() {
+		int checkSize = ensureCorrectMinusSize(size);
+		return posStorageBuffer [checkSize] == SIGNAL_EMPTY;
 	}
 
 	public void addFrame(Vector3 pos, Vector3 rot) {
 		posStorageBuffer [size] = pos;
 		rotStorageBuffer [size] = rot;
-		size ++;
-		if (isFull ()) {
-			bufferDump ();
-		}
+		size = (size + 1) % MAX_SIZE;
 	}
 
 	public Vector3[] readOff() {
 		Vector3[] posRotArray = new Vector3[2];
-		if (hasBeenDumped) {
-			if(size != 0) {
-				posRotArray[0] = posStorageBuffer[size - 1];
-				posRotArray[1] = rotStorageBuffer[size - 1];
-				size--;
-			} else if (storeSize != -1) {
-				posRotArray[0] = posStorage[storeSize];
-				posRotArray[1] = rotStorage[storeSize];
-				storeSize--;
-			}
+		size = ensureCorrectMinusSize (size);
+		if(posStorageBuffer[size] != SIGNAL_EMPTY) {
+			posRotArray[0] = posStorageBuffer[size];
+			posRotArray[1] = rotStorageBuffer[size];
+			posStorageBuffer[size] = SIGNAL_EMPTY;
 		}
 		return posRotArray;
+	}
+
+	private int ensureCorrectMinusSize(int sizeIn) {
+		if (sizeIn == 0) {
+			sizeIn = MAX_SIZE - 1;
+		} else {
+			sizeIn--;
+		}
+		return sizeIn;
 	}
 }
 
@@ -74,10 +64,13 @@ public class CameraMover : MonoBehaviour {
 		yRotation = 0;
 		timeControl = new TimeController ();
 	}
-	
-	// Update is called once per frame
+
+	/* Update is called once per frame
+	 * If T is pressed and timeControl's backing array isn't spent, rewind time
+	 * Else change the object as normal and record the object's rotation and translation in timeControl
+	 */
 	void Update () {
-		if (Input.GetKey (KeyCode.T) && timeControl.hasGottenDumped()) {
+		if (Input.GetKey (KeyCode.T) && !timeControl.isEmpty()) {
 			Vector3[] temp = timeControl.readOff();
 			transform.Translate (-temp[0]);
 			transform.localEulerAngles = temp[1];
