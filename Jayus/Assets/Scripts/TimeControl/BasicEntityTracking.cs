b115d036444498cf4710da6807f916167fc96cc4
BrennenBukovics <brennen.bukovics@gmail.com>
2014-08-03 11:30:15 -0400

    Moved out some code for the rigid body tracker into a new base class to simplify logic in the future.

--- Jayus/Assets/Scripts/TimeControl/BasicEntityTracking.cs
+++ Jayus/Assets/Scripts/TimeControl/BasicEntityTracking.cs
@@ -9,62 +9,34 @@ using UnityEngine;
 
 namespace Jayus.TimeControl
 {
-	public class BasicEntityTracking : ObjectBehavior
+	public class BasicEntityTracking : GenericTracker<RigidBodyState>
 	{
-        private StateTracker<RigidBodyState> _stateTracker { get; set; }
-
-        [IoC.Inject]
-        protected ITimeController _timeControl { get; set; }
-
         public override void Start()
         {
             base.Start();
-
-            //Newing this up instead of injecting it because the current IoC framework does not allow for transient lifestyles
-            _stateTracker = new StateTracker<RigidBodyState>();
-
             if (rigidbody == null)
             {
                 throw new Exception("There is no rigidbody attached to this object! Behavior is invalid!");
             }
         }
 
-        /* Update is called once per frame
-        * If T is pressed and timeControl's backing array isn't spent, rewind time
-        * Else change the object as normal and record the object's rotation and translation in timeControl
-        */
-        protected virtual void Update()
+        protected override void LoadState(RigidBodyState state)
         {
-            if (_timeControl.TimeSpeed < 0)
-            {
-                LoadState();
-            }
-            else
-            {
-                SaveState();
-            }
+            transform.position = state.Position;
+            transform.localEulerAngles = state.Rotation;
+            rigidbody.velocity = state.Velocity;
+            rigidbody.angularVelocity = state.AngularVelocity;
         }
 
-        protected virtual void LoadState()
+        protected override RigidBodyState SaveState()
         {
-            var newState = _stateTracker.ReadPreviousTick();
-
-            transform.position = newState.Position;
-            transform.localEulerAngles = newState.Rotation;
-            rigidbody.velocity = newState.Velocity;
-            rigidbody.angularVelocity = newState.AngularVelocity;
-        }
-
-        protected virtual void SaveState()
-        {
-            var state = new RigidBodyState() 
+            return new RigidBodyState() 
             {
                 Position = transform.position,
                 Rotation = transform.localEulerAngles,
                 Velocity = rigidbody.velocity,
                 AngularVelocity = rigidbody.angularVelocity 
             };
-            _stateTracker.SaveStateForCurrentTick(state);
         }
 	}
 }
---
+++ Jayus/Assets/Scripts/TimeControl/GenericTracker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Jayus.Core;
+using Jayus.ObjectStates;
+
+namespace Jayus.TimeControl
+{
+	public class GenericTracker<T> : ObjectBehavior where T : ObjectState
+	{
+        protected StateTracker<T> _stateTracker { get; set; }
+
+        [IoC.Inject]
+        protected ITimeController _timeControl { get; set; }
+
+        public override void Start()
+        {
+            base.Start();
+
+            //Newing this up instead of injecting it because the current IoC framework does not allow for transient lifestyles
+            _stateTracker = new StateTracker<T>();
+        }
+
+        protected virtual void Update()
+        {
+            if (_timeControl.TimeSpeed < 0)
+            {
+                var newState = _stateTracker.ReadPreviousTick();
+                LoadState(newState);
+            }
+            else
+            {
+                _stateTracker.SaveStateForCurrentTick(SaveState());
+            }
+        }
+
+        protected virtual void LoadState(T state) { }
+        protected virtual T SaveState() { return null; }
+	}
+}
